8ad12e8878fbf3091e8e2db51ff1f38e4443813c
Vladislav Klochkov <kvladislavo2013@gmail.com>
2017-12-26 22:40:59 +0300

    Added UserService, WayService, GeolocationService

---
+++ src/GeolocationService.thrift
@@ -0,0 +1,6 @@
+namespace java com.way.services
+include "types.thrift"
+
+service GeolocationService {
+
+}
---
+++ src/WayService.thrift
@@ -0,0 +1,28 @@
+namespace java com.way.services
+include "types.thrift"
+
+/*
+* Service for working with map and pathes
+*/
+
+service WayService {
+    /*
+    * Adds new point to path
+    */
+    void addPointToPath ( 1: i32 pointId, 2: i32 pathId, 3: string authToken )
+
+    /*
+    * Deletes point from path
+    */
+    void deletePointFromPath ( 1: i32 pointId, 2: i32 pathId, 3: string authToken )
+
+    /*
+    * Creates new path
+    */
+    void createPath ( 1: i32 pathId, 2: i32 userId, 3: string authToken )
+
+    /*
+    * Deletes new path
+    */
+    void deletePath ( 1: i32 pathId, 2: string authToken )
+}
